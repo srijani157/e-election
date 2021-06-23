c05ca339564bba9e9687d94418f4fa6c5679c9a1
srijani157 <srijani157@gmail.com>
2021-06-23 22:52:04 +0530

    toast

--- src/Voter.js
+++ src/Voter.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 import Particles from 'react-particles-js';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Link } from "react-router-dom";
 import eci from "./eci.png";
 import inc from "./inc.png";
@@ -21,6 +23,27 @@ const particlesopt = {
                 }
               }
 class Voter extends Component{
+  constructor(props){  
+    super(props);  
+    this.state = {  
+         count : "0"
+      }   
+  }  
+  handleVoting(party) {  
+    if(this.state.count === "0"){
+      console.log(party);
+      console.log(this.state.count);
+      toast("Thank you for casting your vote!");
+      this.setState({
+        count : "1"
+      })
+    }
+    else{
+      toast.error("Sorry You can cast your vote only once!");
+      console.log(this.state.count);
+    }
+      
+  }  
   render(){
     return (
       <div class="container vot-container">
@@ -49,18 +72,18 @@ class Voter extends Component{
         <h4>Click on the party symbol of your choice!!</h4>
             <div class="row">
                 <div class="col-md-6 col-sm-12 col-lg-6">
-                <button class="btn1"> <img src={bjp}  alt="bjp" width="325" class="party-logo bjp"/></button>
+                <button class="btn1"  onClick={() => this.handleVoting("bjp")}> <img src={bjp}  alt="bjp" width="325" class="party-logo bjp"/></button>
                 </div>
                 <div class="col-md-6 col-sm-12 col-lg-6">
-                <button class="btn1"> <img src={inc}  alt="inc" width="325" class="party-logo inc"/></button>
+                <button class="btn1" onClick={() => this.handleVoting("inc")}> <img src={inc}  alt="inc" width="325" class="party-logo inc"/></button>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6 col-sm-12 col-lg-6">
-                <button class="btn1"> <img src={tmc}  alt="tmc" width="240" class="party-logo tmc"/></button>
+                <button class="btn1" onClick={() => this.handleVoting("tmc")}> <img src={tmc}  alt="tmc" width="240" class="party-logo tmc"/></button>
                 </div>
                 <div class="col-md-6 col-sm-12 col-lg-6">
-                <button class="btn1"> <img src={cpim}  alt="cpim" width="250" class="party-logo cpim"/></button>
+                <button class="btn1" onClick={() => this.handleVoting("cpim")}> <img src={cpim}  alt="cpim" width="250" class="party-logo cpim"/></button>
                 </div>
                 
             </div>
@@ -68,13 +91,14 @@ class Voter extends Component{
                 <div class="col-md-3 col-sm-12 col-lg-3">
                 </div>
                 <div class="col-md-6 col-sm-12 col-lg-6">
-                <button class="btn1"> <img src={nota}  alt="nota" width="250" class="party-logo nota"/></button>
+                <button class="btn1" onClick={() => this.handleVoting("nota")}> <img src={nota}  alt="nota" width="250" class="party-logo nota"/></button>
                 </div>
                 <div class="col-md-3 col-sm-12 col-lg-3">
                 </div>
             </div>
         </div>
     </div>
+    <ToastContainer />
     </div>
     );
   }
